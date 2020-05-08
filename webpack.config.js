b044702e30698381b3b45e6ed0a82427c9bba321
Rob <r.banister.91@gmail.com>
2020-05-08 14:24:57 +0100

    TypeScript Working
    > None strict TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,30 @@ var path = require('path');
 
 module.exports = {
     // Defined entry point
-    entry: './src/index.js',
+    entry: './src/index.tsx',
     mode: "development",
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(t|j)sx?$/,
                 include: path.resolve(__dirname, 'src'),
                 exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
+                loader: 'awesome-typescript-loader',
                 options: {
                     presets: ["@babel/preset-env", "@babel/preset-react"]
                 }
             },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader" },
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
             }
         ]
     },
-    resolve: { extensions: ["*", ".js", ".jsx"] },
+    resolve: { extensions: ["*", ".js", ".jsx", '.ts', '.tsx'] },
     // Defined bundled output location
     output: {
         path: path.resolve(__dirname, "dist/"),
