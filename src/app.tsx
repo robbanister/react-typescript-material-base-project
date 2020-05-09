f5cdb89589e562475e6b01c4c2dd85cbac235e76
Rob <r.banister.91@gmail.com>
2020-05-09 11:32:15 +0100

    Material UI With TypeScript
    > Basic MaterialUI implementation working

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import React, { Component} from "react";
+import Button from '@material-ui/core/Button';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! With React and TypeScript</h1>
+        <h1> Hello, World! With React and TypeScript & MaterialUI</h1>
+				<Button variant="contained" color="secondary">Hello World</Button>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-ReactDOM.render(<App />, document.getElementById("root"));
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+
+ReactDOM.render(
+<ThemeProvider theme={theme}>
+<CssBaseline />
+<App />
+</ThemeProvider>,
+ document.getElementById("root"));
